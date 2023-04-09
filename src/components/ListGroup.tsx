1280595ff4efdf62d63c29f5ccb4a19c64196570
Prathik Anand Krishnan <prathik.anandkrishnan@arup.com>
2023-04-09 23:09:29 +0530

    managing state

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,5 @@
 //import { Fragment } from "react";
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   /*   items = []; */
@@ -9,14 +9,19 @@ function ListGroup() {
         <h1>List</h1>
         <p>No item(s) found</p>
       </>
-    ); */
-
-  /*   const message = items.length === 0 ? <p>No item(s) found</p> : null;
-
+    );
+  const message = items.length === 0 ? <p>No item(s) found</p> : null;
+  
   //inside a function
   const getMessage = () => {
     const message = items.length === 0 ? <p>No item(s) found</p> : null;
   }; */
+
+  //hook; hooks allow us to tap into built-in features in react
+  //so this is called statehook.
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  /*   arr[0]; //variable (selectionIndex)
+  arr[1]; //updater function */
 
   //event handler function
   const handleClick = (event: MouseEvent) => console.log(event);
@@ -28,7 +33,19 @@ function ListGroup() {
       {items.length === 0 && <p>No item(s) found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+          <li
+            /* className="list-group-item active" */
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            /* onClick={handleClick} */
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
