c0a4271ebb49167059f0093b82ec10f60645c50d
Prathik Anand Krishnan <prathik.anandkrishnan@arup.com>
2023-04-09 22:22:56 +0530

    adding src and public files

---
+++ src/Message.tsx
@@ -0,0 +1,8 @@
+//PascalCasing
+function Message() {
+  //JSX: Javascript XML format
+  const name = "Ajay";
+  if (name) return <h1>Hello World {name}</h1>;
+  return <h1>Hello</h1>;
+}
+export default Message;
