8b14a3096342d8285ffb2826871212b8cc0357d1
Prathik Anand Krishnan <prathik.anandkrishnan@arup.com>
2023-04-09 22:39:19 +0530

    Conditional formatting

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,29 @@
 //import { Fragment } from "react";
 
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+  /*   if (items.length == 0)
+    return (
+      <>
+        <h1>List</h1>
+        <p>No item(s) found</p>
+      </>
+    ); */
+
+  /*   const message = items.length === 0 ? <p>No item(s) found</p> : null;
+
+  //inside a function
+  const getMessage = () => {
+    const message = items.length === 0 ? <p>No item(s) found</p> : null;
+  }; */
 
   return (
     <>
       <h1>List</h1>
+      {/* {message} */}
+      {/* {getMessage()} */}
+      {items.length === 0 && <p>No item(s) found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
